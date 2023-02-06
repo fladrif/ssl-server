33e1fb4324b06f61943a41cf859afc64c43312a3
Jonathan Li <comm@renderjon.li>
2023-02-06 15:45:35 -0800

    feat: Add battle jonathan.li

--- start-certbot.sh
+++ start-certbot.sh
@@ -4,6 +4,7 @@ certbot certonly --noninteractive --agree-tos --domains maple.rs --email admin@m
 certbot certonly --noninteractive --agree-tos --domains server.maple.rs --email admin@maple.rs --nginx
 certbot certonly --noninteractive --agree-tos --domains sag.maple.rs --email admin@maple.rs --nginx
 certbot certonly --noninteractive --agree-tos --domains jonathan.li --email webmaster@renderjon.li --nginx
+certbot certonly --noninteractive --agree-tos --domains battle.jonathan.li --email webmaster@renderjon.li --nginx
 certbot certonly --noninteractive --agree-tos --domains mqinternalmartialart.com --email webmaster@renderjon.li --nginx
 
 cp -r /etc/letsencrypt/* /opt/app/letsencrypt/
