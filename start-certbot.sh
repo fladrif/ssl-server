071f6b7df52cf93127b1076e9dcbea8a68021034
Jonathan Li <comm@renderjon.li>
2023-07-10 03:21:04 -0700

    feat: Rachel and Jonathan

--- start-certbot.sh
+++ start-certbot.sh
@@ -5,6 +5,7 @@ certbot certonly --noninteractive --agree-tos --domains server.maple.rs --email 
 certbot certonly --noninteractive --agree-tos --domains sag.maple.rs --email admin@maple.rs --nginx
 certbot certonly --noninteractive --agree-tos --domains jonathan.li --email webmaster@renderjon.li --nginx
 certbot certonly --noninteractive --agree-tos --domains battle.jonathan.li --email webmaster@renderjon.li --nginx
+certbot certonly --noninteractive --agree-tos --domains rachel.jonathan.li --email webmaster@renderjon.li --nginx
 certbot certonly --noninteractive --agree-tos --domains mqinternalmartialart.com --email webmaster@renderjon.li --nginx
 
 cp -r /etc/letsencrypt/* /opt/app/letsencrypt/
